1659df75fff36ff6a4a9109b241b947bd3ce053d
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-03-01 02:08:41 -0300

    camera | renderer

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -2,4 +2,4 @@ import scene from "./basic/Scene.js";
 import camera from "./basic/Camera.js";
 import renderer from "./basic/Renderer.js";
 
-console.log(scene, camera, renderer);
+console.log(scene, camera);
