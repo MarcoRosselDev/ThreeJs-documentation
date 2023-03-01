d6b33e164f38aec527951f0c5197261e88e9b24e
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-03-01 02:26:41 -0300

    cubo rotation eje y

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -8,5 +8,10 @@ scene.add(cube);
 camera.position.set(1, 1, 1);
 camera.lookAt(0, 0, 0);
 
-renderer.render(scene, camera); // esto toma una imagen de la escena
+// renderer.render(scene, camera); // esto toma una imagen de la escena
 // console.log(scene, camera, renderer, cube);
+
+setInterval(() => {
+  cube.rotation.y += 0.01; // esto esta en radianes y un radian = 180 grados, por eso se trabajara con decimas de readianes
+  renderer.render(scene, camera);
+}, 1000 / 60);
