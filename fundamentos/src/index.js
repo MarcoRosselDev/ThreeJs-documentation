7109471ca5ec450e0df52abf9cea5dd0aa9a0e65
MarcoRosselDev <andresmarcorossel@gmail.com>
2023-02-28 22:52:04 -0300

    some mistake fixed

--- fundamentos/src/index.js
+++ fundamentos/src/index.js
@@ -1,3 +1,5 @@
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+
 import scene from "./basic/Scene.js";
 import camera from "./basic/Camera.js";
 import renderer from "./basic/Renderer.js";
